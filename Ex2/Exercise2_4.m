6eeaaf39e1c9d3212571547aaab2dda5a0d945ad
Luke Pugin <lukepugin@gmail.com>
2025-11-19 17:38:04 +0100

    almost done with task 2.4, added figures

--- Ex2/Exercise2_4.m
+++ Ex2/Exercise2_4.m
@@ -1,5 +1,6 @@
 clc;
-clear all;
+clear;
+
 close all;
 format long g;
 
@@ -16,17 +17,42 @@ y_tls = tls_data(:,2);
 y_ibis = ibis_data(:,2);
 
 figure
-plot(t, y_tls, '.-')
+plot(t_tls, y_tls, '.-')
 hold on 
-plot(t, y_ibis, '.-')
+plot(t_ibis, y_ibis, '.-')
+legend('TLS observations','IBIS observations')
+title('Observation plots')
 xlabel('Time [s]')
 ylabel('Displacement [m]')
 
 % determine time-shift
+
 [xcf, lags] = xcorr(y_tls, y_ibis, 'normalized');
+[maximum,delay] = max(abs(xcf));
+shift_samples = lags(delay);
+
 
 figure
 plot(lags, xcf, '.')
+xlabel('Shift / lags')
+ylabel('Cross-corellation value')
+title('Correllation function plot')
+
+freq_est = length(t_tls) / max(t_tls) % 1/s
+s_from_freq_est = 1/freq_est % s
+total_shift_s = shift_samples * s_from_freq_est
+sprintf('integer Shift until maximum corellation: %.i\n\ntime-adjusted shift until maximum corellation: %.3fs',shift_samples,total_shift_s);
 
 % correct time-axis of TLS-measurements 
-% t_tls = ...
+t_tls_corr = t_tls + 2*total_shift_s;
+
+figure
+hold on;
+plot(t_tls,y_tls,'-')
+plot(t_tls_corr,y_tls,'-')
+plot(t_ibis, y_ibis,'-')
+legend('TLS observations','shift-corrected TLS observations','original IBIS time series')
+title('Observation plots (corrected)')
+xlabel('Time [s]')
+ylabel('Displacement [m]')
+hold off;
